d66f02be2202e2e69111dd3b4b2f8283c96a146a
racerf01 <racerf01@mail.ru>
2023-05-11 18:53:48 +0200

    l

--- scripts/AutomaticSett.js
+++
@@ -1,5 +0,0 @@
-function closestNumber(numbers, target) {
-    return numbers.reduce(function(prev, curr) {
-      return (Math.abs(curr - target) < Math.abs(prev - target) ? curr : prev);
-    });
-  }
